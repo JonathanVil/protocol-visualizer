f569292cf515ec2fb1a95d2f9f944d7941ecedd0
Jonathan Antoine Villeret <jvilleret@proton.me>
2026-01-26 15:59:10 +0100

    feat: add dummy text area for protocol declaration

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+    let sourceCode = "";
+
+    function compile() {
+      console.log("Compiling:", sourceCode);
+      alert("Compiling:\n" + sourceCode);
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<textarea
+  bind:value={sourceCode}
+  placeholder="Write your protocol here..."
+></textarea>
+
+<button on:click={compile}>
+  Compile
+</button>
