211e2469488b81483c0c4748ced0dd9abcdb420f
VernesNemo <mawc@itu.dk>
2026-02-05 16:33:31 +0100

    feat: Add message transit time
    Co-authored-by: Marius Holm <marho@itu.dk>

--- protocol-simulator/src/lib/types.js
+++ protocol-simulator/src/lib/types.js
@@ -1,6 +1,6 @@
 ﻿/** @typedef {new (id: number) => { id: string }} ActorConstructor */
 
-/** @typedef {{ source: number, destination: number, type: string}} Message */
+/** @typedef {{ source: number, destination: number, type: string, transitSteps: number, elapsedSteps: number}} Message */
 // TODO: allow messages to carry a value
 
 
