4122a2ac91974bba9cc05ede59e2704f99957185
Marius Holm <marho@itu.dk>
2026-02-16 12:35:59 +0100

    refactor: move settingpanel into svelte component

---
+++ protocol-simulator/src/lib/SettingsPanel.svelte
@@ -0,0 +1,47 @@
+﻿<script>
+    import {getStepSize, setStepSize, setTransitbounds} from "$lib/protocolUtils.js";
+
+
+    // the actual values of these is not stored here, so these are not important until updated by user
+    let transitLowerInput = 8
+    let transitUpperInput = 12
+
+    let stepSizeInput = getStepSize();
+    export let stepSizeUpdated = false;
+    function setStepSizeInput() {
+        setStepSize(stepSizeInput);
+        stepSizeUpdated = true;
+    }
+
+    function setTransitTimeInput() {
+        setTransitbounds(transitUpperInput, transitLowerInput);
+    }
+</script>
+
+<div id="settingspanel" class="hidden absolute top-22 right-1 rounded-lg w-1/7 h-2/5 bg-[#91B7C7]/16 border">
+    <div class="flex flex-col gap-3">
+        <div class="font-medium">
+            <p class="">Step size</p>
+        </div>
+        <div>
+            <input type="range" class="w-3/4" min="10" max="1000" bind:value={stepSizeInput} on:input={setStepSizeInput}>
+            <p>{stepSizeInput}</p>
+        </div>
+        <div class="font-medium">
+            <p>Transit time</p>
+        </div>
+        <div class="flex">
+            <div class="flex flex-col">
+                <p>Min</p>
+                <input type="range" class="w-1/2" min="1" max="{transitUpperInput - 1}" bind:value={transitLowerInput} on:input={setTransitTimeInput}>
+                <p>{transitLowerInput}</p>
+            </div>
+            <div>
+                <p>Max</p>
+                <input type="range" class="w-1/2" min="{transitLowerInput}" max="50" bind:value={transitUpperInput} on:input={setTransitTimeInput}>
+                <p>{transitUpperInput}</p>
+            </div>
+
+        </div>
+    </div>
+</div>
--- protocol-simulator/src/routes/+page.svelte
+++ protocol-simulator/src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     import MonacoEditer from "$lib/MonacoEditer.svelte";
+    import SettingsPanel from "$lib/SettingsPanel.svelte";
     import Graph  from "$lib/Graph.svelte";
     import {Queue} from '$lib/Queue.js';
     import ManualMessageComponent from "$lib/ManualMessageComponent.svelte";
-    import {getTransitTime, setTransitbounds, getNextMessageId, parseProtocolCode, getStepSize, setStepSize} from "$lib/protocolUtils.js";
+    import {getTransitTime, getNextMessageId, parseProtocolCode, getStepSize} from "$lib/protocolUtils.js";
     import Icon from '@iconify/svelte';
     import {onMount} from "svelte";
 
@@ -36,11 +37,6 @@
     /** @type number */
     let intervalId;
 
-    // the actual values of these is not stored here, so these are not important until updated by user
-    let transitLowerInput = 8
-    let transitUpperInput = 12
-
-    let stepSizeInput = getStepSize();
     let stepSizeUpdated = false;
     let paused = true;
 
@@ -90,14 +86,7 @@
         paused = true;
     }
 
-    function setStepSizeInput() {
-        setStepSize(stepSizeInput);
-        stepSizeUpdated = true;
-    }
 
-    function setTransitTimeInput() {
-        setTransitbounds(transitUpperInput, transitLowerInput);
-    }
 
     function resetSimulation() {
         clearInterval(intervalId);
@@ -266,9 +255,6 @@
     <a href="https://github.com/JonathanVil/protocol-visualizer" aria-label="GitHub">
         <i class="fa fa-github" style="font-size:36px"></i>
     </a>
-
-
-
 </header>
 
 <!--Dotted graph (background)-->
@@ -304,34 +290,7 @@
 
 
 <!--Settings block-->
-<div id="settingspanel" class="hidden absolute top-22 right-1 rounded-lg w-1/7 h-2/5 bg-[#91B7C7]/16 border">
-    <div class="flex flex-col gap-3">
-        <div class="font-medium">
-            <p class="">Step size</p>
-        </div>
-        <div>
-            <input type="range" class="w-3/4" min="10" max="1000" bind:value={stepSizeInput} on:input={setStepSizeInput}>
-            <p>{stepSizeInput}</p>
-        </div>
-        <div class="font-medium">
-            <p>Transit time</p>
-        </div>
-        <div class="flex">
-            <div class="flex flex-col">
-                <p>Min</p>
-                <input type="range" class="w-1/2" min="1" max="{transitUpperInput - 1}" bind:value={transitLowerInput} on:input={setTransitTimeInput}>
-                <p>{transitLowerInput}</p>
-            </div>
-            <div>
-                <p>Max</p>
-                <input type="range" class="w-1/2" min="{transitLowerInput}" max="50" bind:value={transitUpperInput} on:input={setTransitTimeInput}>
-                <p>{transitUpperInput}</p>
-            </div>
-
-        </div>
-    </div>
-
-</div>
+<SettingsPanel bind:stepSizeUpdated={stepSizeUpdated}></SettingsPanel>
 
 <!--Message block-->
 <div >
