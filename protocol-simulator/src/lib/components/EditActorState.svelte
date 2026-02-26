9e9fd8072cc0c7f1b91b419b44c47cb3a5c2236a
Jonathan Antoine Villeret <jvilleret@proton.me>
2026-02-26 14:23:04 +0100

    cleanup: remove unused import

--- protocol-simulator/src/lib/components/EditActorState.svelte
+++ protocol-simulator/src/lib/components/EditActorState.svelte
@@ -1,7 +1,4 @@
 <script>
-    // --- inline edit popup state ---
-    import ActorStatePopper from "$lib/components/ActorStatePopper.svelte";
-
     /** @type {string | null} */
     export let editingKey = null;
 
