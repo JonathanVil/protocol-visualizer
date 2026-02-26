b9e77b82cb6be8bdfa1d09cc4a581f92c7889a76
Marius Holm <marho@itu.dk>
2026-02-26 20:41:42 +0100

    feat: create MessagePopper.svelte component

--- protocol-simulator/src/lib/components/MessagePopper.svelte
+++ protocol-simulator/src/lib/components/MessagePopper.svelte
@@ -1,8 +1,54 @@
 ﻿<script>
+    /** @typedef {import('$lib/types.js').Message} Message */
 
+
+
+    /** @type {Message} */
+    export let message;
+
+    let delay = 0;
+
+
+    function deliverMessage() {
+
+    }
 
 </script>
 
-<p>
-    awdawd
-</p>
+
+<div class="pointer-events-auto relative whitespace-nowrap rounded-lg border border-white/10 bg-slate-900/90 px-2 py-1.5 text-[16px] leading-[1.2] text-white shadow-[0_8px_20px_rgba(0,0,0,0.35)]">
+    <!-- Message data -->
+    <div class="flex flex-col items-center text-center">
+        <p class="items-center font-bold">Message</p>
+        <div>
+            <div class="text-xs">ID: {message.id}</div>
+            <div class="">{message.source} ⟶ {message.destination}</div>
+        </div>
+    </div>
+
+    <hr class="h-px my-2 bg-white border-0">
+    <!-- Methods -->
+    <div class="flex flex-row gap-2 items-center text-center">
+        <button class=" bg-blue-600 text-white rounded hover:bg-blue-700 w-25 h-10 text-base flex text-center justify-center items-center"
+                on:click={deliverMessage}>
+            Deliver
+        </button>
+        <button class=" bg-blue-600 text-white rounded hover:bg-blue-700 w-25 h-10 text-base flex text-center justify-center items-center"
+                on:click={deliverMessage}>
+            Drop
+        </button>
+    </div>
+
+    <div class="mt-2 flex flex-row items-center text-center gap-2">
+        <button class="bg-blue-600 text-white rounded hover:bg-blue-800 w-25 h-10 text-base flex text-center justify-center items-center"
+                on:click={deliverMessage}>
+            Delay
+        </button>
+        <input class="border p-1 h-10 w-12 rounded relative z-[9999] bg-blue-800" id="from" bind:value={delay} placeholder="ID" />
+        <p>
+            ticks
+        </p>
+    </div>
+
+
+</div>
