f5033acb846fa94023d017b9e404185ddfc7809b
Marius Holm <marho@itu.dk>
2026-04-03 11:04:21 +0200

    feat: init raft

---
+++ protocol-simulator/src/protocols/Raft.js
@@ -0,0 +1,25 @@
+﻿// https://www.usenix.org/system/files/conference/atc14/atc14-paper-ongaro.pdf
+class Actor {
+    constructor(id) {
+        this.id = id;
+
+        // STATE
+        this.currentTerm = 0;
+        this.votedFor = null;
+        this.log = [];
+        this.commitIndex = 0;
+        this.lastApplied = 0;
+        this.nextIndex = {};
+        this.matchIndex = {};
+
+
+
+    }
+
+    receive(msg) {
+
+
+    }
+}
+
+return Actor;
