77dadc3a78dfd315af8e7292ac3e716a05bd6d37
Marius Holm <marho@itu.dk>
2026-04-07 14:33:14 +0200

    feat: implemented RequestVote
    Co-authored-by: VernesNemo <mawc@itu.dk>

--- protocol-simulator/src/protocols/Raft.js
+++ protocol-simulator/src/protocols/Raft.js
@@ -18,7 +18,50 @@ class Actor {
 
     receive(msg) {
 
+        if (msg.type === 'VOTEREQUEST') {
+            let vote = false;
+            // Reply false if term < currentTerm (§5.1)
+            if (msg.data.term < this.currentTerm) {
+                vote = false;
+            } else if (this.votedFor === null || this.votedFor === msg.data.candidateId) {
 
+                // Check if candidate's log is at least as up-to-date as receiver's log
+                let upToDate = false;
+                let lastLogTerm = this.log.length > 0 ? this.log[this.commitIndex - 1].term : 0;
+
+                if (lastLogTerm < msg.data.lastLogTerm) {
+                    // The last entry in the candidate's log has a higher term than the last entry in the receiver's log, see $5.4.1
+                    upToDate = true;
+                } else if (lastLogTerm === msg.data.lastLogTerm && this.commitIndex <= msg.data.lastLogIndex) {
+                    // The last entry in both logs have the same term AND the candidate's log is at least as long as receiver's log
+                    upToDate = true;
+                }
+                if (upToDate) {
+                    this.votedFor = msg.data.candidateId;
+                    this.currentTerm = msg.data.term;
+                    vote = true;
+                }
+            }
+            send(this.id, msg.from, [this.currentTerm, vote], 'VOTEREPLY');
+        }
+
+    }
+
+    //send a VOTEREQUEST to all other actors. Called upon election timeout.
+    requestVote() {
+        this.votedFor = this.id
+        for (const actor in actors) {
+            if (actor.id !== this.id) {
+                let lastLogTerm = this.log.length > 0 ? this.log[this.commitIndex - 1].term : 0;
+                let voteRequestData = {
+                    term: this.currentTerm,
+                    candidateId: this.id,
+                    lastLogIndex: this.commitIndex,
+                    lastLogTerm: lastLogTerm
+                }
+                send(this.id, actor.id, voteRequestData, 'VOTEREQUEST');
+            }
+        }
     }
 }
 
