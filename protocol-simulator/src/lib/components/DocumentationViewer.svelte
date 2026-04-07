5a83e5e223fb2c5f7cfe91f2357062113a328f55
Jonathan Antoine Villeret <jvilleret@proton.me>
2026-04-07 14:24:00 +0200

    feat: render docs as markdown

--- protocol-simulator/src/lib/components/DocumentationViewer.svelte
+++ protocol-simulator/src/lib/components/DocumentationViewer.svelte
@@ -1,19 +1,22 @@
 <script>
   import { onMount } from 'svelte';
-  let markdown = '';
+  import {marked} from "marked";
+
+
+  let source = '';
 
   onMount(async () => {
     const res = await fetch('/BASICS.md');
-    markdown = await res.text();
+    source = await res.text();
   });
 </script>
 
 <div class="flex h-full flex-col">
-    <div class="flex items-center justify-between gap-3 rounded-t-md border border-b-0 border-gray-300 bg-gray-100 px-3 py-2">
+    <div class="rounded-t-md border border-b-0 border-gray-300 bg-gray-100 px-3 py-2">
         <h1 class="text-lg font-medium">Guide</h1>
     </div>
 
-    <div class="overflow-scroll max-h-[calc(100vh-20rem)] shadow-xl p-2 rounded-b-md bg-white border border-gray-300">
-        <pre>{markdown}</pre>
+    <div class="overflow-scroll max-h-[calc(100vh-20rem)] shadow-xl p-2 rounded-b-md bg-white border border-gray-300 prose">
+        {@html marked.parse(source)}
     </div>
 </div>
