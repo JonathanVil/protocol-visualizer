423b1be259405d2062cbe220d6dcafee3eb7d951
Marius Holm <marho@itu.dk>
2026-04-09 15:08:18 +0200

    feat: raft
    Co-authored-by: VernesNemo <mawc@itu.dk>
    Co-authored-by: Jonathan Antoine Villeret <jvilleret@proton.me>

--- protocol-simulator/src/protocols/Raft.js
+++ protocol-simulator/src/protocols/Raft.js
@@ -10,8 +10,8 @@ class Actor {
         this.log = [];
         this.commitIndex = 0;
         this.lastApplied = 0;
-        this.nextIndex = {};
-        this.matchIndex = {};
+        this.nextIndex = [];
+        this.matchIndex = [];
         this.votes = 0;
 
         this.electionTimeout();
@@ -20,10 +20,16 @@ class Actor {
     receive(msg) {
 
         if (msg.type === 'VOTE_REQUEST') {
+            /*
+            msg.data: { term: number; candidateId: number; lastLogIndex: number; lastLogTerm: number; }
+             */
+
             let vote = false;
 
-            // a voterequest in a new term, reset.
-            if (msg.data.term > this.currentTerm) {this.votedFor = null;}
+            // a vote request in a new term, reset.
+            if (msg.data.term > this.currentTerm) {
+                this.votedFor = null;
+            }
 
             // Reply false if term < currentTerm (§5.1)
             if (msg.data.term < this.currentTerm) {
@@ -50,58 +56,120 @@ class Actor {
             send(this.id, msg.from, 'VOTE_REPLY', [this.currentTerm, vote] );
 
         } else if (msg.type === 'VOTE_REPLY') {
+            /*
+            msg.data: { term: number; voteGranted: boolean; }
+             */
+
             this.votes++;
             // If votes received from majority of actors. It wins the election (a)
             if (this.votes > Math.floor(getActors() + 1 / 2)) {
-                this.state = "LEADER";
-                this.votes = 0;
+                this.becomeLeader();
                 // BEGIN SENDING HEARTBEATS
             }
         } else if (msg.type === 'APPEND_ENTRIES_REQUEST') {
+            /*
+            msg.data: { term: number; prevLogIndex: number; prevLogTerm: number; entries: []; leaderCommit: number; }
+             */
+
+            this.electionTimeout();
 
             // "While waiting for votes, a candidate may receive an AppendEntries RPC from another server claiming to be leader. If the leader’s term (included in its RPC) is at least
             // as large as the candidate’s current term, then the candidate recognizes the leader as legitimate and returns to follower state." (b)
             if (this.state === "CANDIDATE" && this.currentTerm <= msg.data.term) {
-                this.state = "FOLLOWER";
-                this.votes = 0;
+                this.becomeFollower();
             }
 
             // 1. Reply false if term < currentTerm (§5.1)
             if (msg.data.term < this.currentTerm) {
-                send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', false);
+                let result = {
+                    term: this.currentTerm,
+                    success: false
+                }
+                send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', result);
                 return;
             }
 
             //  2. Reply false if log doesn’t contain an entry at prevLogIndex whose term matches prevLogTerm (§5.3)
             if (this.log.length <= msg.data.prevLogIndex || this.log[msg.data.prevLogIndex].term !== msg.data.prevLogTerm) {
-                send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', false);
+                let result = {
+                    term: this.currentTerm,
+                    success: false
+                }
+                send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', result);
                 return;
             }
 
             // 3. If an existing entry conflicts with a new one (same index but different terms), delete the existing entry and all that follow it (§5.3)
-            
+            if (this.log.length > msg.data.prevLogIndex + 1 && this.log[msg.data.prevLogIndex + 1].term !== msg.data.term) {
+                this.log = this.log.slice(0, msg.data.prevLogIndex + 1);
+            }
 
+            // 4. Append any new entries not already in the log
+            // [1, 2, 3] <- [2, 3, 4] = [1, 2, 3, 2, 3, 4]
+            this.log = this.log.concat(msg.data.entries);
+
+            let result = {
+                term: this.currentTerm,
+                success: true
+            }
+            send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', result);
+
+        } else if (msg.type === "APPEND_ENTRIES_REPLY") {
+            /*
+            msg.data: { term: number; success: boolean; }
+             */
+
+            // if the follower's term is higher than current leaders term
+            if (msg.data.term > this.currentTerm) {
+                this.becomeFollower();
+            }
+
+            if (msg.data.success) {
+                this.nextIndex[msg.from] += 1;
+            } else {
+                this.nextIndex[msg.from] -= 1;
+                this.sendAppendEntries(msg.from)
+            }
 
         }
+    }
 
+    becomeFollower() {
+        this.state = "FOLLOWER";
+        this.votes = 0;
+        this.nodeColor = 'blue';
+    }
 
+    becomeLeader() {
+        this.state = "LEADER";
+        this.votes = 0;
+
+        this.nextIndex = Array(getActors()).fill(this.log.length);
+        this.matchIndex = Array(getActors()).fill(0);
+        this.nodeColor = 'red';
+        this.broadcastAppendEntries()
+    }
+
+    becomeCandidate() {
+        this.state = "CANDIDATE";
+        this.currentTerm = this.currentTerm + 1;
+        this.votedFor = this.id
+        this.nodeColor = 'yellow';
     }
 
     electionTimeout() {
-        let randomTimeout = (150 + Math.floor(Math.random() * 150)) / 3; // Random timeout between 50 and 100 ticks
-        timeout(this, randomTimeout, this.requestVote);
+        let randomTimeout = (100 + Math.floor(Math.random() * 300)); // Random timeout between 50 and 100 ticks
+        timeout(this, randomTimeout, this.requestVotes);
     }
 
-    // send a VOTEREQUEST to all other actors. Called upon election timeout.
+    // send a VOTE_REQUEST to all other actors. Called upon election timeout.
     // "A candidate continues in this state until one of three things happens:
     // (a) it wins the election,
     // (b) another server establishes itself as leader, or
     // (c) a period of time goes by with no winner."
-    requestVote() {
-        if (this.votedFor !== null && this.votedFor !== this.id) return; // already voted for someone else, do not start election.
-        this.currentTerm = this.currentTerm + 1;
-        this.state = "CANDIDATE";
-        this.votedFor = this.id
+    requestVotes() {
+        if (this.votedF or !== null && this.votedFor !== this.id) return; // already voted for someone else, do not start election.
+        this.becomeCandidate();
         this.electionTimeout() // (c)
         let lastLogTerm = this.log.length > 0 ? this.log[this.commitIndex - 1].term : 0;
         for (let actorId = 0; actorId < getActors(); actorId++) {
@@ -117,20 +185,30 @@ class Actor {
         }
     }
 
-    // send a APPENDENTRIESEQUEST from leader to followers. Also used for heartbeat.
-    sendAppendEntries() {
-        let appendEntryData = {
-            term: this.currentTerm,
-            leaderId: this.id,
-            prevLogIndex: this.prevLogIndex,
-            prevLogTerm: this.prevLogTerm,
-            entries: this.log
-        }
+    // send a APPEND_ENTRIES_REQUEST from leader to followers. Also used for heartbeat.
+    broadcastAppendEntries() {
+        if (!this.state === "LEADER") {return}
+
         for (let actorId = 0; actorId < getActors(); actorId++) {
             if (actorId !== this.id) {
-                send(this.id, actorId, "APPEND_ENTRIES_REQUEST", appendEntryData);
+                this.sendAppendEntry(actorId);
+
             }
         }
+        timeout(this, 50, this.broadcastAppendEntries); //might want to adjust all the timeouts to make it seem less busy
+    }
+
+    sendAppendEntries(actorId) {
+        let prevLogIndex = this.nextIndex[actorId] - 1;
+        let prevLogTerm = this.log[this.nextIndex[actorId] - 1].term;
+        let appendEntriesData = {
+            term: this.currentTerm,
+            leaderId: this.id,
+            prevLogIndex: prevLogIndex,
+            prevLogTerm: prevLogTerm,
+            entries: this.log.slice(this.nextIndex[actorId])
+        }
+        send(this.id, actorId, "APPEND_ENTRIES_REQUEST", appendEntriesData);
     }
 }
 
