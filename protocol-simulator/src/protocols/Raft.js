d945bf0ac10846ab717fc6c27328fbe134ca00f7
Marius Holm <marho@itu.dk>
2026-04-09 14:02:56 +0200

    feat: raft
    Co-authored-by: VernesNemo <mawc@itu.dk>
    Co-authored-by: Jonathan Antoine Villeret <jvilleret@proton.me>

--- protocol-simulator/src/protocols/Raft.js
+++ protocol-simulator/src/protocols/Raft.js
@@ -19,12 +19,16 @@ class Actor {
 
     receive(msg) {
 
-        if (msg.type === 'VOTEREQUEST') {
+        if (msg.type === 'VOTE_REQUEST') {
             let vote = false;
+
+            // a voterequest in a new term, reset.
+            if (msg.data.term > this.currentTerm) {this.votedFor = null;}
+
             // Reply false if term < currentTerm (§5.1)
             if (msg.data.term < this.currentTerm) {
                 vote = false;
-            } else if (this.votedFor === null || this.votedFor === msg.data.candidateId) {
+            } else if (this.votedFor === null || this.votedFor === msg.data.candidateId) { // "Each server will vote for at most one candidate in a given term"
 
                 // Check if candidate's log is at least as up-to-date as receiver's log
                 let upToDate = false;
@@ -43,14 +47,40 @@ class Actor {
                     vote = true;
                 }
             }
-            send(this.id, msg.from, 'VOTEREPLY', [this.currentTerm, vote] );
-        } else if (msg.type === 'VOTEREPLY') {
+            send(this.id, msg.from, 'VOTE_REPLY', [this.currentTerm, vote] );
+
+        } else if (msg.type === 'VOTE_REPLY') {
             this.votes++;
-            // If votes received from majority of actors
+            // If votes received from majority of actors. It wins the election (a)
             if (this.votes > Math.floor(getActors() + 1 / 2)) {
                 this.state = "LEADER";
+                this.votes = 0;
+                // BEGIN SENDING HEARTBEATS
             }
-        } else if (msg.type === 'APPENDENTRIESEQUEST') {
+        } else if (msg.type === 'APPEND_ENTRIES_REQUEST') {
+
+            // "While waiting for votes, a candidate may receive an AppendEntries RPC from another server claiming to be leader. If the leader’s term (included in its RPC) is at least
+            // as large as the candidate’s current term, then the candidate recognizes the leader as legitimate and returns to follower state." (b)
+            if (this.state === "CANDIDATE" && this.currentTerm <= msg.data.term) {
+                this.state = "FOLLOWER";
+                this.votes = 0;
+            }
+
+            // 1. Reply false if term < currentTerm (§5.1)
+            if (msg.data.term < this.currentTerm) {
+                send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', false);
+                return;
+            }
+
+            //  2. Reply false if log doesn’t contain an entry at prevLogIndex whose term matches prevLogTerm (§5.3)
+            if (this.log.length <= msg.data.prevLogIndex || this.log[msg.data.prevLogIndex].term !== msg.data.prevLogTerm) {
+                send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', false);
+                return;
+            }
+
+            // 3. If an existing entry conflicts with a new one (same index but different terms), delete the existing entry and all that follow it (§5.3)
+            
+
 
         }
 
@@ -62,12 +92,17 @@ class Actor {
         timeout(this, randomTimeout, this.requestVote);
     }
 
-    //send a VOTEREQUEST to all other actors. Called upon election timeout.
+    // send a VOTEREQUEST to all other actors. Called upon election timeout.
+    // "A candidate continues in this state until one of three things happens:
+    // (a) it wins the election,
+    // (b) another server establishes itself as leader, or
+    // (c) a period of time goes by with no winner."
     requestVote() {
-        if (this.votedFor !== null) return; // already voted for someone else, do not start election
+        if (this.votedFor !== null && this.votedFor !== this.id) return; // already voted for someone else, do not start election.
+        this.currentTerm = this.currentTerm + 1;
         this.state = "CANDIDATE";
         this.votedFor = this.id
-        this.currentTerm = this.currentTerm + 1;
+        this.electionTimeout() // (c)
         let lastLogTerm = this.log.length > 0 ? this.log[this.commitIndex - 1].term : 0;
         for (let actorId = 0; actorId < getActors(); actorId++) {
             if (actorId !== this.id) {
@@ -77,20 +112,24 @@ class Actor {
                     lastLogIndex: this.commitIndex,
                     lastLogTerm: lastLogTerm
                 }
-                send(this.id, actorId, "VOTEREQUEST", voteRequestData);
+                send(this.id, actorId, "VOTE_REQUEST", voteRequestData);
             }
         }
     }
 
+    // send a APPENDENTRIESEQUEST from leader to followers. Also used for heartbeat.
     sendAppendEntries() {
-
-
         let appendEntryData = {
             term: this.currentTerm,
-            candidateId: this.id,
+            leaderId: this.id,
             prevLogIndex: this.prevLogIndex,
             prevLogTerm: this.prevLogTerm,
             entries: this.log
+        }
+        for (let actorId = 0; actorId < getActors(); actorId++) {
+            if (actorId !== this.id) {
+                send(this.id, actorId, "APPEND_ENTRIES_REQUEST", appendEntryData);
+            }
         }
     }
 }
