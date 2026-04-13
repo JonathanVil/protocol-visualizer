be85c2545702ca54f3e6dd30140257dcec8ee4fc
Marius Holm <marho@itu.dk>
2026-04-13 10:49:50 +0200

    feat: implement commitIndex and matchIndex in Raft
    Co-authored-by: VernesNemo <mawc@itu.dk>
    Co-authored-by: Jonathan Antoine Villeret <jvilleret@proton.me>

--- protocol-simulator/src/protocols/Raft.js
+++ protocol-simulator/src/protocols/Raft.js
@@ -9,9 +9,9 @@ class Actor {
         this.votedFor = null;
         this.log = [];
         this.commitIndex = 0;
-        this.lastApplied = 0; //TODO
+        this.lastApplied = 0;
         this.nextIndex = [];
-        this.matchIndex = []; //TODO
+        this.matchIndex = [];
         this.votes = 0;
         this.timeoutId = null
         this.startElectionTimeout();
@@ -55,19 +55,25 @@ class Actor {
                     vote = true;
                 }
             }
-            send(this.id, msg.from, 'VOTE_REPLY', [this.currentTerm, vote] );
+            send(this.id, msg.from, 'VOTE_REPLY', { term: this.currentTerm, voteGranted: vote } );
 
         } else if (msg.type === 'VOTE_REPLY') {
             /*
             msg.data: { term: number; voteGranted: boolean; }
              */
 
-            this.votes++;
-            // If votes received from majority of actors. It wins the election (a)
-            if (this.votes >= Math.floor(getActors() / 2) + 1) {
-                this.becomeLeader();
-                // BEGIN SENDING HEARTBEATS
+            if (msg.data.voteGranted) {
+                this.votes++;
+                // If votes received from majority of actors. It wins the election (a)
+                if (this.votes >= Math.floor(getActors() / 2) + 1) {
+                    this.becomeLeader();
+                    // BEGIN SENDING HEARTBEATS
+                }
+            } else if (msg.data.term > this.currentTerm) {
+                this.currentTerm = msg.data.term;
             }
+
+
         } else if (msg.type === 'APPEND_ENTRIES_REQUEST') {
             /*
             msg.data: { term: number; prevLogIndex: number; prevLogTerm: number; entries: []; leaderCommit: number; }
@@ -102,7 +108,9 @@ class Actor {
             }
 
             // 3. If an existing entry conflicts with a new one (same index but different terms), delete the existing entry and all that follow it (§5.3)
-            if (msg.data.prevLogIndex === null) {this.log = []} // if the whole log is bad
+            if (msg.data.prevLogIndex === null) { // if the whole log is bad
+                this.log = []
+            }
             if (this.log.length > msg.data.prevLogIndex + 1 && this.log[msg.data.prevLogIndex + 1].term !== msg.data.term) {
                 this.log = this.log.slice(0, msg.data.prevLogIndex + 1);
             }
@@ -115,7 +123,17 @@ class Actor {
                 term: this.currentTerm,
                 success: true
             }
+
+            if (msg.data.leaderCommit > this.commitIndex) {
+                this.commitIndex = Math.min(msg.data.leaderCommit, this.log.length);
+                if (this.commitIndex > this.lastApplied) {
+                    this.lastApplied++;
+                    // TODO: apply log[this.lastApplied] to state machine
+                }
+            }
+
             send(this.id, msg.from, 'APPEND_ENTRIES_REPLY', result);
+
 
         } else if (msg.type === "APPEND_ENTRIES_REPLY") {
             /*
@@ -127,10 +145,31 @@ class Actor {
                 this.becomeFollower();
             }
 
+            // Only true, if the followers log matches the leaders log.
             if (msg.data.success) {
-                this.nextIndex[msg.from] += 1;
+                this.nextIndex[msg.from] = this.log.length;
+                this.matchIndex[msg.from] = this.log[msg.from];
+
+                // If there exists an N such that N > commitIndex ,a majority
+                // of match Index[i] ≥= N,and log[N].term == currentTerm:
+                // set commitIndex = N (§5.3,§5.4).
+                // - Sort matchIndex to find the median matchIndex. Decrement if terms does not match && i > commitIndex
+                let sorted = this.matchIndex.toSorted();
+                for (let i = sorted[Math.floor(getActors() / 2)]; i > this.commitIndex; i--) {
+                    if (this.log[i].term === this.currentTerm) {
+                        this.commitIndex = i;
+                        break;
+                    }
+                }
+
+                if (this.commitIndex > this.lastApplied) {
+                    this.lastApplied++;
+                    // TODO: apply log[this.lastApplied] to state machine
+                }
             } else {
-                this.nextIndex[msg.from] -= 1;
+                if (this.nextIndex[msg.from] > 0) {
+                    this.nextIndex[msg.from] -= 1;
+                }
                 this.sendAppendEntries(msg.from)
             }
 
@@ -146,7 +185,6 @@ class Actor {
     becomeLeader() {
         this.state = "LEADER";
         this.votes = 0;
-
         this.nextIndex = Array(getActors()).fill(this.log.length);
         this.matchIndex = Array(getActors()).fill(0);
         deleteTimeout(this.timeoutId);
@@ -194,12 +232,11 @@ class Actor {
 
     // send a APPEND_ENTRIES_REQUEST from leader to followers. Also used for heartbeat.
     broadcastAppendEntries() {
-        if (!this.state === "LEADER") {return}
+        if (!this.state === "LEADER") return;
 
         for (let actorId = 0; actorId < getActors(); actorId++) {
             if (actorId !== this.id) {
                 this.sendAppendEntries(actorId);
-
             }
         }
 
@@ -210,10 +247,10 @@ class Actor {
         let prevLogIndex = null
         let prevLogTerm = null
         let entries = []
-        if (this.nextIndex[actorId] !== 0)  {
+        if (this.nextIndex[actorId] > 0)  {
             prevLogIndex = this.nextIndex[actorId] - 1;
             prevLogTerm = this.log[prevLogIndex].term;
-            entries = this.log.slice(this.nextIndex[actorId])
+            entries = this.log.slice(this.nextIndex[actorId]);
         }
 
         let appendEntriesData = {
@@ -221,7 +258,8 @@ class Actor {
             leaderId: this.id,
             prevLogIndex: prevLogIndex,
             prevLogTerm: prevLogTerm,
-            entries: entries
+            entries: entries,
+            leaderCommit: this.commitIndex
         }
         send(this.id, actorId, "APPEND_ENTRIES_REQUEST", appendEntriesData);
     }
